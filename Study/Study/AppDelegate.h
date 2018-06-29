5a1e273334f441c1997b47f47b3f32033349ab39
hongqingWang <272338444@qq.com>
2018-06-29 11:44:17 +0800

    创建项目
    创建项目

---
+++ Study/Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Study
+//
+//  Created by 王红庆 on 2018/6/29.
+//  Copyright © 2018年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
